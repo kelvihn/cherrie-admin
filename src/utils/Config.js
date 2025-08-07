94ce5700852df33cca15a1397f1e19c02a4c1dd1
tega-roy-dev <tega.roy@adtmtech.com>
2025-08-07 15:10:28 +0100

    updated base url

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,2 +1,2 @@
 export const key = "00000";
-export const baseURL = "https://cherrie-backend-render-production.up.railway.app/";
+export const baseURL = "https://cherrie-backend-render.onrender.com/";
